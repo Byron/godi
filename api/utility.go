0593cfea72236a61d90e0031cc02a6a8e80fc9b0
Sebastian Thiel <byronimo@gmail.com>
2014-07-19 00:55:53 +0200

    Now only one buffer is used per ChannelReader
    This means there may only be blocking reader operation, but it should be alright.
    It's faster now as well

--- api/utility.go
+++ api/utility.go
@@ -8,6 +8,8 @@ import (
 
 // Size for allocated buffers
 const bufSize = 64 * 1024
+
+// Actually, this must remain 0 for our sync to work, right now, without pool
 const readChannelSize = 0
 
 // Similar to MultiWriter, but assumes writes never fail, and provides the same buffer
@@ -37,14 +39,13 @@ func UncheckedParallelMultiWriter(writers ...io.Writer) io.Writer {
 
 // The result of a read operation, similar to what Reader.Read returns
 type readResult struct {
-	buf *[bufSize]byte
+	buf []byte
 	n   int
 	err error
 }
 
 type ReadChannelController struct {
 	c chan ChannelReader
-	p sync.Pool
 }
 
 type WriteChannelController struct {
@@ -74,8 +75,13 @@ type ChannelReader struct {
 
 	// Sync availability of our buffers, as we cam't use a sync.Pool due tot he slice truncation we have to do
 	// p.Put(p.Get()[:5]) will put in a different slice, even though they point to the same array. It's not possible
-	// to retrieve the underlying
-	// wg
+	// to retrieve the underlying array.
+	// When sending an array pointer directly, the same issue occurred. Don't know why ... hangs returned ...
+	// Hanging occurred most of the time, but not always, which points at some async issue, maybe a bug in Pool ?
+	wg *sync.WaitGroup
+
+	// buffers we swap to allow reading while the writer is still busy writing, at least once ...
+	buf *[bufSize]byte
 }
 
 type ChannelWriter struct {
@@ -111,11 +117,17 @@ func (r *ReadChannelController) Channel() chan<- ChannelReader {
 func (r *ReadChannelController) NewChannelReaderFromPath(path string) ChannelReader {
 	// NOTE: size of this channel controls how much we can cache into memory before we block
 	// as the consumer doesn't keep up
-	return ChannelReader{r, path, nil, make(chan readResult, readChannelSize)}
+	return ChannelReader{r, path, nil, make(chan readResult, readChannelSize),
+		&sync.WaitGroup{},
+		new([bufSize]byte),
+	}
 }
 
 func (r *ReadChannelController) NewChannelReaderFromReader(reader io.Reader) ChannelReader {
-	return ChannelReader{r, "", reader, make(chan readResult, readChannelSize)}
+	return ChannelReader{r, "", reader, make(chan readResult, readChannelSize),
+		&sync.WaitGroup{},
+		new([bufSize]byte),
+	}
 }
 
 // Allows to use a ChannelReader as source for io.Copy operations
@@ -131,10 +143,11 @@ func (p *ChannelReader) WriteTo(w io.Writer) (n int64, err error) {
 	for res := range p.results {
 		// Write what's possible
 		if res.n > 0 {
-			written, err = w.Write(res.buf[:res.n])
-			p.ctrl.p.Put(res.buf)
+			written, err = w.Write(res.buf)
 			n += int64(written)
+			p.wg.Done()
 		}
+
 		// I could think of plenty of assertion here ... but there is no such thing
 	}
 
@@ -152,7 +165,6 @@ func NewReadChannelController(nprocs int) ReadChannelController {
 
 	ctrl := ReadChannelController{
 		make(chan ChannelReader, nprocs),
-		sync.Pool{New: func() interface{} { return new([bufSize]byte) }},
 	}
 
 	for i := 0; i < nprocs; i++ {
@@ -163,6 +175,7 @@ func NewReadChannelController(nprocs int) ReadChannelController {
 				if info.reader == nil {
 					info.reader, err = os.Open(info.path)
 					if err != nil {
+						info.wg.Add(1)
 						info.results <- readResult{nil, 0, err}
 						continue
 					}
@@ -172,9 +185,11 @@ func NewReadChannelController(nprocs int) ReadChannelController {
 				var nread int
 				for {
 					// The buffer will be put back by the one reading from the channel (e.g. in WriteTo()) !
-					buf := ctrl.p.Get().(*[bufSize]byte)
-					nread, err = info.reader.Read(buf[:])
-					info.results <- readResult{buf, nread, err}
+					// wait until writer from previous iteration is done using the buffer
+					info.wg.Wait()
+					nread, err = info.reader.Read(info.buf[:])
+					info.wg.Add(1)
+					info.results <- readResult{info.buf[:nread], nread, err}
 					// we send all results, but abort if the reader is done for whichever reason
 					if err != nil {
 						break
