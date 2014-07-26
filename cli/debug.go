adaf5ccff2af989b928983a071dd4c34af0fb840
Sebastian Thiel <byronimo@gmail.com>
2014-07-26 09:52:29 +0200

    Added CPU profile option

--- cli/debug.go
+++ cli/debug.go
@@ -11,12 +11,14 @@ import (
 
 const (
 	memProfileFlagName = "memory-profile"
+	cpuProfileFlagName = "cpu-profile"
 )
 
 func AddAdditinalFlags(a *cli.App) {
 	// nop
-	a.Flags = append(a.Flags, cli.StringFlag{
-		memProfileFlagName, "", "If set, the given file will contain the memory profile of the previous run"},
+	a.Flags = append(a.Flags,
+		cli.StringFlag{memProfileFlagName, "", "If set, the given file will contain the memory profile of the previous run"},
+		cli.StringFlag{cpuProfileFlagName, "", "If set, the given file will contain the CPU profile of the previous run"},
 	)
 }
 
@@ -33,9 +35,23 @@ func CliFinishApp(c *cli.Context) error {
 		pprof.WriteHeapProfile(f)
 		f.Close()
 	}
+
+	// CPU Profile
+	///////////////
+	if len(c.GlobalString(cpuProfileFlagName)) > 0 {
+		pprof.StopCPUProfile()
+	}
 	return nil
 }
 
 func parseAdditionalFlags(c *cli.Context) error {
+	cpuf := c.GlobalString(cpuProfileFlagName)
+	if len(cpuf) > 0 {
+		f, err := os.Create(cpuf)
+		if err != nil {
+			return err
+		}
+		pprof.StartCPUProfile(f)
+	}
 	return nil
 }
